f7f5a25fa4cf39475ec15508004e58321dc2e508
Blagos11 <nikita_belyakov_13@mail.ru>
2024-06-10 22:04:59 +0300

    Add files via upload

---
+++ Лаба 3 структ Беляков.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <deque>
+#include <vector>
+
+enum Color { WHITE, BLACK };
+
+std::vector<Color> arrange_cards(int n) {
+    std::deque<Color> deque;
+    for (int i = n; i > 0; --i) {
+        if (i % 2 == 1) {
+            deque.push_front(WHITE);
+        }
+        else {
+            deque.push_front(BLACK);
+        }
+        if (i > 1) {
+            deque.push_front(deque.back());
+            deque.pop_back();
+        }
+    }
+    return std::vector<Color>(deque.begin(), deque.end());
+}
+
+void print_deck(const std::vector<Color>& deck) {
+    for (Color card : deck) {
+        if (card == WHITE) {
+            std::cout << "WHITE ";
+        }
+        else {
+            std::cout << "BLACK ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    int n;
+    std::cout << "Введите количество карточек: ";
+    std::cin >> n;
+
+    if (n % 2 != 0) {
+        std::cout << "Невозможно чередовать карточки по цвету с нечетным количеством." << std::endl;
+        return 1;
+    }
+
+    std::vector<Color> initial_deck = arrange_cards(n);
+    std::cout << "Начальное расположение карточек в стопке: ";
+    print_deck(initial_deck);
+
+    return 0;
+}
